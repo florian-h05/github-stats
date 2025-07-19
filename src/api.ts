florian-h05/github-stats

commit cf915e5cdf71b6dbfcea8834a33bf816151d5e0d

Abort fetching older PRs if milestone is missing for >= 100 PRs
This greatly reduces fetch times as we don't need to fetch all PRs anymore to filter afterwards.

src/api.ts
@@ -53,18 +53,25 @@ export async function fetchAllCommits(
 
 /**
  * Fetches all pull requests for a given repository.
+ *
+ * If the milestone argument is provided, the method will abort fetching older PRs as soon as at least 100 PRs without the milestone have been fetched.
+ * This way it is avoided that all PRs have to be fetched if they should be filtered by milestone.
+ *
  * @param octokit An instance of Octokit.
  * @param owner The repository owner's username.
  * @param repo The repository name.
  * @param state The state of the pull requests to fetch.
+ * @param milestone The milestone to filter by.
  */
 export async function fetchAllPullRequests(
   octokit: Octokit,
   owner: string,
   repo: string,
   state: "open" | "merged" | "closed" | "all" = "all",
+  milestone?: string,
 ) {
   const results = [];
+  let abortCountdown = 100;
   for await (const { data } of octokit.paginate.iterator(
     octokit.rest.pulls.list,
     {
@@ -72,12 +79,17 @@ export async function fetchAllPullRequests(
       repo,
       state: state === "merged" ? "closed" : state,
       per_page: 100,
+      sort: "created",
+      direction: "desc",
+      milestone,
     },
   )) {
-    results.push(...data);
-  }
-  if (state === "merged") {
-    return results.filter((pr) => pr.merged_at !== null);
+    for (const pr of data) {
+      if (milestone && pr.milestone?.title !== milestone) abortCountdown--;
+      if (state === "merged" && pr.merged_at === null) continue;
+      results.push(pr);
+    }
+    if (abortCountdown <= 0) break;
   }
   return results;
 }

src/utils.ts
@@ -69,6 +69,7 @@ export async function getPullRequestStats(
     repo.owner,
     repo.name,
     "merged",
+    milestone,
   );
 
   const stats: PullRequestStats = {
@@ -79,10 +80,6 @@ export async function getPullRequestStats(
   };
 
   for (const pr of pullRequests) {
-    if (milestone && pr.milestone?.title !== milestone) {
-      continue;
-    }
-
     stats.pr_count++;
 
     if (pr.labels.length > 0) {

commit e3f203f63722b618221498f6a7b1bedbfbcdad7f

Fix pull_request_stats milestone filter missing
Regression from previous commit cf915e5cdf71b6dbfcea8834a33bf816151d5e0d.

src/api.ts
@@ -62,7 +62,7 @@ export async function fetchAllCommits(
 /**
  * Fetches all pull requests for a given repository.
  *
- * If the milestone argument is provided, the method will abort fetching older PRs as soon as at least 100 PRs without the milestone have been fetched.
+ * If the milestone argument is provided, the method will abort fetching older PRs as soon as at least 500 PRs without the milestone have been fetched.
  * This way it is avoided that all PRs have to be fetched if they should be filtered by milestone.
  *
  * @param octokit An instance of Octokit.
@@ -81,7 +81,7 @@ export async function fetchAllPullRequests(
   console.debug(`Fetching all pull requests for ${owner}/${repo} ...`);
   console.time("fetchAllPullRequests");
   const results = [];
-  let abortCountdown = 100;
+  let abortCountdown = 500;
   for await (const { data } of octokit.paginate.iterator(
     octokit.rest.pulls.list,
     {
@@ -89,13 +89,16 @@ export async function fetchAllPullRequests(
       repo,
       state: state === "merged" ? "closed" : state,
       per_page: 100,
-      sort: "created",
+      sort: "updated",
       direction: "desc",
       milestone,
     },
   )) {
     for (const pr of data) {
-      if (milestone && pr.milestone?.title !== milestone) abortCountdown--;
+      if (milestone && pr.milestone?.title !== milestone) {
+        abortCountdown--;
+        continue;
+      }
       if (state === "merged" && pr.merged_at === null) continue;
       results.push(pr);
     }